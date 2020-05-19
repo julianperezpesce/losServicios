5fb0a512f5929d28f02f0fb2788f552bbf35b5e7
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-05-19 20:57:05 -0300

    se creo, clase user.ts. y se la implemento en articulo detalle.ts. También en el Servicio articulos.ts

--- src/app/articulo-detalle/articulo-detalle.component.ts
+++ src/app/articulo-detalle/articulo-detalle.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ArticulosService } from '../services/articulos.service';
 import { Articulo } from '../models/articulo';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-articulo-detalle',
@@ -10,10 +11,15 @@ import { Articulo } from '../models/articulo';
 export class ArticuloDetalleComponent implements OnInit {
 
   articulo: Articulo = new Articulo();
+  usuario: User = new User();
+
   constructor(private articuloInyectado: ArticulosService) { }
 
   ngOnInit(): void {
     this.articulo = this.articuloInyectado.articulo;
+    this.articuloInyectado.leerUsuario(this.articulo.userId).subscribe((usuarioDesdeApi)=>{
+      this.usuario = usuarioDesdeApi;
+    })
   }
 
   
--- src/app/services/articulos.service.ts
+++ src/app/services/articulos.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { Articulo } from '../models/articulo';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticulosService {
   articulo: Articulo = new Articulo();
+  ruta: string = 'https://jsonplaceholder.typicode.com';
   constructor(private http: HttpClient) {
 
    }
@@ -15,6 +17,11 @@ export class ArticulosService {
    leerNoticias(): Observable<Articulo[]>
    {
      //this.http.get<Array<Articulo>>
-     return this.http.get<Articulo[]>('https://jsonplaceholder.typicode.com/posts');
+     return this.http.get<Articulo[]>(this.ruta + '/posts');
+   }
+
+   leerUsuario(userId: number): Observable<User>
+   {
+      return this.http.get<User>(this.ruta + '/users' + userId);
    }
 }
