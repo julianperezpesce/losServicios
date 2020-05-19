4993d5f657253a7f1fe5daf34ea8dd0de1d3983b
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-05-19 12:41:45 -0300

    modificacion en home.ts y home.html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Articulo } from '../models/articulo';
+import { UsuarioService } from '../services/usuario.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  articulos: Array<Articulo> = new Array<Articulo>();
 
-  constructor() { }
+  constructor(public usuarioInyectado: UsuarioService) { }
 
   ngOnInit(): void {
+    this.articulos.push({
+      titulo: "Curso de Net Core",
+      descripcion: "Lorem ipsum dolor sit amet consectetur adipiscing elit feugiat dis vel, nulla id proin vitae cursus enim quisque quam luctus, ac scelerisque convallis neque nibh semper non egestas bibendum.",
+      fecha: new Date(),
+      usuario: "${this.usuarioInyectado.usuario.nombre} ${this.usuarioInyectado.usuario.apellido}" ,
+    },
+    {
+      titulo: "Curso de Angular",
+      descripcion: "Lorem ipsum dolor sit amet consectetur adipiscing elit feugiat dis vel, nulla id proin vitae cursus enim quisque quam luctus, ac scelerisque convallis neque nibh semper non egestas bibendum.",
+      fecha: new Date('17/03/2020'),
+      usuario: "${this.usuarioInyectado.usuario.nombre} ${this.usuarioInyectado.usuario.apellido}" ,
+    },
+    {
+      titulo: "Curso de Flutter",
+      descripcion: "Lorem ipsum dolor sit amet consectetur adipiscing elit feugiat dis vel, nulla id proin vitae cursus enim quisque quam luctus, ac scelerisque convallis neque nibh semper non egestas bibendum.",
+      fecha: new Date('30/06/2019'),
+      usuario: "${this.usuarioInyectado.usuario.nombre} ${this.usuarioInyectado.usuario.apellido}" ,
+    },)
   }
 
 }
---
+++ src/app/models/articulo.ts
@@ -0,0 +1,6 @@
+export interface Articulo{
+    titulo: string,
+    descripcion: string,
+    fecha: Date,
+    usuario: string,
+}
