3e446639e3f41798ae335613652317995da0ea8e
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-05-19 12:49:50 -0300

    Correción de $usuario en home.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,19 +17,19 @@ export class HomeComponent implements OnInit {
       titulo: "Curso de Net Core",
       descripcion: "Lorem ipsum dolor sit amet consectetur adipiscing elit feugiat dis vel, nulla id proin vitae cursus enim quisque quam luctus, ac scelerisque convallis neque nibh semper non egestas bibendum.",
       fecha: new Date(),
-      usuario: "${this.usuarioInyectado.usuario.nombre} ${this.usuarioInyectado.usuario.apellido}" ,
+      usuario: `${this.usuarioInyectado.usuario.nombre} ${this.usuarioInyectado.usuario.apellido}`,
     },
     {
       titulo: "Curso de Angular",
       descripcion: "Lorem ipsum dolor sit amet consectetur adipiscing elit feugiat dis vel, nulla id proin vitae cursus enim quisque quam luctus, ac scelerisque convallis neque nibh semper non egestas bibendum.",
-      fecha: new Date('17/03/2020'),
-      usuario: "${this.usuarioInyectado.usuario.nombre} ${this.usuarioInyectado.usuario.apellido}" ,
+      fecha: new Date('03/17/2020'),
+      usuario: `${this.usuarioInyectado.usuario.nombre} ${this.usuarioInyectado.usuario.apellido}`,
     },
     {
       titulo: "Curso de Flutter",
       descripcion: "Lorem ipsum dolor sit amet consectetur adipiscing elit feugiat dis vel, nulla id proin vitae cursus enim quisque quam luctus, ac scelerisque convallis neque nibh semper non egestas bibendum.",
-      fecha: new Date('30/06/2019'),
-      usuario: "${this.usuarioInyectado.usuario.nombre} ${this.usuarioInyectado.usuario.apellido}" ,
+      fecha: new Date('06/30/2019'),
+      usuario:` ${this.usuarioInyectado.usuario.nombre} ${this.usuarioInyectado.usuario.apellido}`,
     },)
   }
 
